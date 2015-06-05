e71a6ce5b732b5ace4cadde42b813ca51678d6dd
Andrej Sinicyn <rarogit@gmail.com>
2015-06-05 12:28:23 +0200

    - Changing layout to use sizers so GUI redesign is easier. - Resizing window so German translations do fit. - Updating translations.

--- translations/OPOT_de_DE.ts
+++ translations/OPOT_de_DE.ts
@@ -9,114 +9,114 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="45"/>
+        <location filename="../mainwindow.ui" line="965"/>
         <source>Boot Fastboot</source>
         <translation>Im Fastboot Modus neustarten</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="61"/>
+        <location filename="../mainwindow.ui" line="975"/>
         <source>Reboot</source>
         <translation>Gerät neustarten</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="77"/>
+        <location filename="../mainwindow.ui" line="985"/>
         <source>Boot Recovery</source>
         <translation>Wiederherstellungs Modus starten</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="93"/>
+        <location filename="../mainwindow.ui" line="995"/>
         <source>Backup</source>
         <translation>Sicherung erstellen</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="109"/>
+        <location filename="../mainwindow.ui" line="1005"/>
         <source>OEM unlock</source>
         <translation>Bootloader entsperren</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="125"/>
+        <location filename="../mainwindow.ui" line="1015"/>
         <source>Restore</source>
         <translation>Sicherung wiederherstellen</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="141"/>
+        <location filename="../mainwindow.ui" line="1025"/>
         <source>Flash ROM*</source>
         <translation>Betriebssystem installieren*</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="157"/>
+        <location filename="../mainwindow.ui" line="1035"/>
         <source>Reboot [Fastboot]</source>
         <translation>Aus Fastboot Modus neustarten</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="173"/>
+        <location filename="../mainwindow.ui" line="1045"/>
         <source>Device list</source>
         <translation>Geräteliste</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="192"/>
+        <location filename="../mainwindow.ui" line="1058"/>
         <source>Root</source>
         <translation>Root</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="206"/>
+        <location filename="../mainwindow.ui" line="67"/>
         <source>This application was developed by Bostrot. I am not responsible for damage that may arise on your device. This is a completly free program and it is not allowed to earn or make money in any way with this application. 
 
 </source>
         <translation>Dieses Tool wurde von Bostrot entwickelt. Ich hafte nicht für Schäden, die an ihrem Gerät unter Nutzung dieses Tools entstehen können. Diese Anwendung ist komplett kostenlos und nich dafür bestimmt Geld zu verdienen.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="230"/>
+        <location filename="../mainwindow.ui" line="51"/>
         <source>*If you want to install a new operating system you have to OEM unlock your device first.</source>
         <translation>*Um ein neues Betriebssystem zu installieren muss der Bootloader entsperrt sein.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="664"/>
+        <location filename="../mainwindow.ui" line="512"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://forums.oneplus.net/threads/oneplus-one-toolkit-new.311774/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;OnePlus One Forum&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1098"/>
+        <location filename="../mainwindow.ui" line="940"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;http://www.p-devs.info/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Developer website&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1116"/>
+        <location filename="../mainwindow.ui" line="1079"/>
         <source>Language</source>
         <translation>Sprache</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1123"/>
+        <location filename="../mainwindow.ui" line="1086"/>
         <source>Help</source>
         <translation>Hilfe</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1130"/>
+        <location filename="../mainwindow.ui" line="1093"/>
         <source>Update</source>
         <translation>Update</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1141"/>
+        <location filename="../mainwindow.ui" line="1104"/>
         <source>German</source>
         <translation>Deutsch</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1146"/>
+        <location filename="../mainwindow.ui" line="1109"/>
         <source>English</source>
         <translation>Englisch</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1151"/>
+        <location filename="../mainwindow.ui" line="1114"/>
         <source>About</source>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1156"/>
+        <location filename="../mainwindow.ui" line="1119"/>
         <source>Feedback</source>
         <translation>Feedback</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1161"/>
+        <location filename="../mainwindow.ui" line="1124"/>
         <source>Download newest version to Desktop</source>
         <translation>Nach Updates suchen...</translation>
     </message>
